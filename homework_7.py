3c3c838843897f42db8607c2369e2ff84a70da3c
Dima_Radchenko <tairbr1985@gmail.com>
2021-01-30 17:08:51 +0300

    Added function

---
+++ homework_7.py
@@ -0,0 +1,58 @@
+def inch(n):  # 1 дюйм = 2.54 сантиметр
+    resault_cm = n * 2.54
+    return resault_cm
+
+
+def centimeter(n):  # 1 сантиметр = 0.3937007874016 дюйм
+    resault_inch = n * 0.3937007874016
+    return resault_inch
+
+
+def mile(n):  # 1 миля = 1.609344 километр
+    resault_km = n * 1.609344
+    return resault_km
+
+
+def km(n):  # 1 километр = 0.6213711922373 миля
+    resault_mile = n * 0.6213711922373
+    return resault_mile
+
+
+def lb(n):  # 1 фунт = 0.45359237 килограмм
+    resault_kg = n * 0.45359237
+    return resault_kg
+
+
+def kg(n):  # 1 килограмм = 2.44192843875 Фунт
+    resault_lb = n * 2.44192843875
+    return resault_lb
+
+
+def ounce(n):  # 1 унция = 28.349523125 грамм
+    resault_gram = n * 28.349523125
+    return resault_gram
+
+
+def gram(n):  # 1 грамм = 0.03527396194958 унция
+    resault_ounce = n * 0.03527396194958
+    return resault_ounce
+
+
+def gallon(n):  # 1 галлон = 3.785411784 литр
+    resault_liters = n * 3.785411784
+    return resault_liters
+
+
+def liters_gl(n):  # 1 литр = 0.2641720523581 галлон
+    resault_gallon = n * 0.2641720523581
+    return resault_gallon
+
+
+def pint(n):  # 1 пинта США = 0.473176473 литр
+    resault_liters = n * 0.473176473
+    return resault_liters
+
+
+def liters_pn(n):  # 1 литр = 2.113376418865 пинта
+    resault_pint = n * 2.113376418865
+    return resault_pint
